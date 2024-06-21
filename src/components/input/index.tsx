af3ef3eaac39f8d0b446200131d0122651bd4ab9
debora <debora.carvalho@ufba.br>
2024-06-20 21:23:24 -0300

    Creating the Input component and initializing the login component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface InputProps {
+  text?: string;
+  type?: string;
+  password?: boolean;
+  value?: string;
+  error?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input = ({ text, value, error, onChange, type = 'text' }: InputProps) => {
+  return (
+    <div>
+      {text && <label>{text}</label>}
+      <input type={type} value={value} onChange={onChange} />
+      {!!error && <p> {error}</p>}
+    </div>
+  );
+}
