b31b740cfa434dc4329b6a7ac34ea7dadf718348
debora <debora.carvalho@ufba.br>
2024-06-20 23:05:16 -0300

    adding login validation, email and password fields

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,19 +1,48 @@
 import React, { useState } from 'react';
 import { Input } from '../input';
 import { Button } from '../button';
+import { isValidEmail, isValidPassword } from '../../utils/strings-utils';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const validateFields = () => {
+    setEmailError('');
+    setPasswordError('');
+
+    if (!email.trim()) {
+      setEmailError('Campo obrigatório.');
+    } else if (!isValidEmail(email)) {
+      setEmailError('O email informado é inválido.');
+    }
+
+    if (!password.trim()) {
+      setPasswordError('Campo obrigatório.');
+    } else if (password.length < 7) {
+      setPasswordError('A senha deve ter pelo menos 7 caracteres.');
+    } else if (!isValidPassword(password)) {
+      setPasswordError('A senha deve ter pelo menos um dígito e uma letra.');
+    }
+  };
 
   const handleSubmit = () => {
-    alert('Usuário logado com sucesso');
+    validateFields();
   };
   return (
     <div>
       <h1>Bem Vindo a TaqTile</h1>
-      <Input text="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Input text="Senha" type="password" password value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Input text="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} error={emailError} />
+      <Input
+        text="Senha"
+        type="password"
+        password
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        error={passwordError}
+      />
       <div>
         <Button onClick={handleSubmit}>Entrar</Button>
       </div>
