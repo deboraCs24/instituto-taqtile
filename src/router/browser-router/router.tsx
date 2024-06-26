d36c6d58dc6e18d1d5af6bb7514ad567d5459176
debora <debora.carvalho@ufba.br>
2024-06-26 01:04:31 -0300

    creation of the user details screen and added the route to the user details screen when clicking on an item in the user list screen and a button to return to the user list

--- src/router/browser-router/router.tsx
+++ src/router/browser-router/router.tsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
     path: '/users/:userId',
     element: <UserDetailsPage />,
   },
+  {
+    path: '/users/:userId',
+    element: <UserDetailsPage />,
+  },
 ]);
 
 export { RouterProvider, router };
