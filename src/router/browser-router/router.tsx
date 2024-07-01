c7f5341f4bdb2a02e258d64961eea8277d9a9c79
debora <debora.carvalho@ufba.br>
2024-07-01 13:58:02 -0300

    removing the log and changing the variable name from birthDateObj to parsedBirthDate and changing the component name

--- src/router/browser-router/router.tsx
+++ src/router/browser-router/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Login } from '../../components/login';
-import { AddCreateUser } from '../../components/add-user';
+import { UserRegistrationForm } from '../../components/add-user';
 import { HomePage } from './pages/home';
 
 const router = createBrowserRouter([
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/addUser',
-    element: <AddCreateUser onSuccess={() => console.log('Usuário adicionado com sucesso!')} />,
+    element: <UserRegistrationForm onSuccess={() => console.log('Usuário adicionado com sucesso!')} />,
   },
 ]);
 
